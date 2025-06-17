1976dc14dc8e352f404c95d7d5bbd7294523e7a5
Ballon14 <iqbal140605@Gmail.com>
2025-06-17 15:20:22 +0700

    add db

---
+++ src/database/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    
+    private static String URL = "jdbc:mysql://100.96.165.28:3306/javadb";
+    private static String USER = "iqbal";
+    private static String PASSWORD = "iqbal";
+    
+    public static Connection connect() {
+        Connection connection = null;
+        
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database connected successfully");
+        } catch (SQLException e) {
+            System.err.println("Error connecting to database: " + e.getMessage());
+        }
+        
+        return connection;
+    }
+}
